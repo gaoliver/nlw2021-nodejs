397c6b2e3eefb37ced3a97db4a1fc833680661c3
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-04-22 01:09:12 -0300

    NLW - Terceiro dia

--- routes.ts
+++ routes.ts
@@ -1,10 +1,22 @@
-import { Router } from "express"
+import { Router } from "express";
+import { MessagesController } from "./src/controllers/MessagesController";
 import { SettingsController } from "./src/controllers/SettingsController";
+import { UsersController } from "./src/controllers/UsersController";
 
 const routes = Router();
 
-const settingsController = new SettingsController
+const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
-routes.post("/settings", settingsController.create)
+// Settings
+routes.post("/settings", settingsController.create);
+
+// Users
+routes.post("/users", usersController.create);
+
+// Messages
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
 
 export { routes };
